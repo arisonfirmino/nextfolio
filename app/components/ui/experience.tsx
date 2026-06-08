383776caf4db9d46bd3dbaa0c71b1b5e83dd9021
arisonfirmino <arsnfrm@email.com>
2026-06-08 00:42:39 -0300

    refactor: simplificar layout da seção de experiências

--- app/components/ui/experience.tsx
+++ app/components/ui/experience.tsx
@@ -6,29 +6,23 @@ import { ExperienceTypes } from "@/app/types";
 
 function Experience({ experience }: { experience: ExperienceTypes }) {
   const title = experience.properties.title.title[0].plain_text;
-  const subtitle = experience.properties.subtitle.rich_text[0].plain_text;
   const organization =
     experience.properties.organization.rich_text[0].plain_text;
   const periodStart = formatedDate(experience.properties.period.date.start);
   const periodEnd = experience.properties.period.date.end
     ? formatedDate(experience.properties.period.date.end)
-    : "Atual";
+    : "Presente";
 
   return (
-    <div className="grid gap-1 md:grid-cols-2 md:gap-8">
-      <div>
-        <p className="text-base font-bold">{title}</p>
-        <span className="text-muted-foreground text-xs">{subtitle}</span>
-      </div>
+    <div className="grid">
+      <p className="text-base font-bold">{title}</p>
 
-      <div>
-        <p className="text-base font-bold">{organization}</p>
-
-        <div className="text-muted-foreground flex items-center gap-4 text-xs capitalize">
-          <span>{periodStart}</span>
-          <MinusIcon />
-          <span>{periodEnd}</span>
-        </div>
+      <div className="text-muted-foreground flex w-full items-center gap-4 text-xs capitalize">
+        <span className="line-clamp-1">{organization}</span>
+        <MinusIcon />
+        <span className="line-clamp-1 w-full max-w-[40%]">
+          {periodStart} - {periodEnd}
+        </span>
       </div>
     </div>
   );
--- app/helpers/formatedDate.ts
+++ app/helpers/formatedDate.ts
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export const formatedDate = (date: string) => {
-  return format(date, "MMM yyyy", {
+  return format(date, "MM/yyyy", {
     locale: ptBR,
   });
 };
